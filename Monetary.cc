51a87133e365b4ab04dc8ad38ae25a29c3a50730
omidefarda <hasna648@student.liu.se>
2013-10-04 21:08:32 +0200

    Update Monetary.cc

--- Monetary.cc
+++ Monetary.cc
@@ -1,4 +1,4 @@
-//#ifndef H_Monetary
+
 //#define H_monetary
 //#include <monetary_error>
 #include <iostream>
@@ -7,19 +7,19 @@
 
 using namespace std;
 
-string valuta;
-int enhet;
-int hundradelar;
+
 class money
 {
-public:
+private:
   string valuta;
   int enhet;
   int hundradelar;
+public:
 
   // Konstruktor utan argument
   money()
   {
+    this ->valuta="";
     this ->enhet=0;
     this ->hundradelar=0;      
   }
@@ -43,24 +43,61 @@ public:
   //money(const char*);
   ~money() = default;
   money& operator=(const char*);
-
-
-  //operatoroverlagring används för att kunna använda +/- etc, även Rvalue och lvalue är för addition.
-
-  /*  money (string valuta, int enhet, int hundradelar)
-  {
-    this ->valuta=valuta;
-    this ->enhet=enhet;
-    this ->hundradelar=hundradelar;
-    }*/
+  
+  //Print med ostream
   void print(ostream &out);
-  };
+  
+  //Sammansättning
+  money& operator+(money&);
+  money& operator=(money&);
 
+  //Jämförelse
+  bool operator==(money&);
+  bool operator<(money&);
+};
 //print osv
 void money::print(ostream &out)
 {
   out << valuta << " " << enhet << "." << hundradelar << endl;
 }
+
+//Sammansättning
+money& money::operator+(money& adderare)
+{
+  if (valuta == adderare.valuta || valuta == "" || adderare.valuta == "")
+    {
+      enhet = enhet + adderare.enhet;
+      hundradelar = hundradelar + adderare.hundradelar;      
+    }
+  else
+    {
+      cout << "Kan inte summera olika valutor" << endl;
+    }
+}
+money& money::operator=(money& tilldelare)
+{
+  if (valuta==tilldelare.valuta || tilldelare.valuta=="")
+    {
+      enhet= tilldelare.enhet;
+      hundradelar=tilldelare.hundradelar;
+     }
+  else if(valuta=="")
+    {
+      valuta=tilldelare.valuta;
+      enhet= tilldelare.enhet;
+      hundradelar=tilldelare.hundradelar;
+    }      
+  else
+    {
+      cout<< "HEJ"<< endl;
+    }
+}
+//Jämförelse
+bool operator=(const money&, const money&);
+
+
+
+
 
 int main(void)
 {
@@ -80,6 +117,20 @@ int main(void)
   m6.print(cout);
   m7.print(cout);
   m8.print(cout);
+  cout << "\n";
+  m1 + m5;
+  m5 + m6;
+  m1.print(cout);
+  m5.print(cout);
+  cout << "\n";
+  m4 = m5;
+  m4.print(cout);
+  m4 = m3;
+  m4.print(cout);
+  m1 = m6;
+  m1.print(cout);
+  m4 = m6;
+  m4.print(cout);
   return 0;
 }
 
