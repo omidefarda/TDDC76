4edba5df34bd37bed36509f46cc71a4f2dd0526d
omidefarda <hasna648@student.liu.se>
2013-10-06 23:40:41 +0200

    Update monetary_error.h

--- monetary_error.h
+++ monetary_error.h
@@ -5,7 +5,7 @@
 class monetary_error : public std::logic_error {
 public:
    // Constructor
-   monetary_error(const string msg) : std::logic_error(msg) {};
+   monetary_error(const string& msg) : std::logic_error(msg) {};
 };
  
 #endif
