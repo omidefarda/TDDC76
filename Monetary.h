20e3aa8ae40a594e022cd33ea47e78f1df710faf
omidefarda <hasna648@student.liu.se>
2013-10-06 22:19:09 +0200

    Update Monetary.h

--- Monetary.h
+++ Monetary.h
@@ -55,44 +55,7 @@ public:
   bool operator==(money&);
   bool operator<(money&);
 };
-//print osv
-void money::print(ostream &out)
-{
-  out << valuta << " " << enhet << "." << hundradelar << endl;
-}
 
-//Sammansättning
-money& money::operator+(money& adderare)
-{
-  if (valuta == adderare.valuta || valuta == "" || adderare.valuta == "")
-    {
-      enhet = enhet + adderare.enhet;
-      hundradelar = hundradelar + adderare.hundradelar;      
-    }
-  else
-    {
-      throw ;
-    }
-}
-money& money::operator=(money& tilldelare)
-{
-  if (valuta==tilldelare.valuta || tilldelare.valuta=="")
-    {
-      enhet= tilldelare.enhet;
-      hundradelar=tilldelare.hundradelar;
-     }
-  else if(valuta=="")
-    {
-      valuta=tilldelare.valuta;
-      enhet= tilldelare.enhet;
-      hundradelar=tilldelare.hundradelar;
-    }      
-  else
-    {
-      cout<< "HEJ"<< endl;
-    }
-}
-//Jämförelse
-//bool operator=(const money&, const money&);
+
 
 #endif
