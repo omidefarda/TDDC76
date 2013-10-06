d8484c21cac98313511e27540669f1ae93b49b19
omidefarda <hasna648@student.liu.se>
2013-10-06 23:32:05 +0200

    Update monetary_error.cc

--- monetary_error.cc
+++ monetary_error.cc
@@ -1,16 +1,11 @@
-#include <iostream>
 #include "monetary_error.h"
 
 using namespace std;
 
-void fun(){
   throw monetary_error();
-}
 
-int main(){
   try{
-    fun();
-  } catch (monetary_error & ex){
+      } catch (monetary_error & ex){
     cout << ex.what() << endl;
   }
-}
+
