a1b7615ee0db6b0653f76733208167bcb7dc5608
omidefarda <hasna648@student.liu.se>
2013-10-16 14:19:33 +0200

    Update lab1-5.cc

--- lab1-5.cc
+++ lab1-5.cc
@@ -1,20 +1,43 @@
-#include "list.h"
-#include <string>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <list>
 using namespace std;
 
 int main ()
 {
-  string namn;
-  list namnlista;
-  list::node* ptr;
-  ptr = namnlista.name(Ada);
-  namnlista.addnode(ptr);
-  ptr = namnlista.name(Beda);
-  namnlista.addnode(ptr);
-  while(cin >> namn)
+  string textfil;
+  string ord;
+  cin >> textfil;
+  list<string> ordlista;
+  fstream textfilen (textfil);
+  if (textfilen.is_open())
     {
-      ptr = namnlista.name(namn);
-      namnlista.addnode(ptr);
-    
-    } 
+      while (getline (textfilen, ord))
+	{
+	  ordlista.insert(ordlista.begin(), ord);
+	}
+      textfilen.close();
+      cout << "välj PRINT eller REVERSE_PRINT" << endl;
+      cin >> ord;
+      if (ord == "p")
+	{
+	  for ( list<string>::iterator it=ordlista.begin(); it != ordlista.end(); ++it)
+	    {
+	      cout << *it << endl;
+	    }
+	}
+      else if (ord == "r")
+	{
+	  for ( list<string>::reverse_iterator rit=ordlista.rbegin(); rit != ordlista.rend() ; ++rit)
+	    {
+	      cout<< *rit<< endl;
+	    }
+	}
+      else
+	cout << "Kan inte det kommandot";
+    }
+  else 
+    cout << "Kan inte öppna filen" << endl;
+  return 0;
+}
