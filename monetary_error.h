37a4b8d0a2e538216134cbed009580b6166a3fad
omidefarda <hasna648@student.liu.se>
2013-11-13 18:25:49 +0100

    Update monetary_error.h

--- monetary_error.h
+++ monetary_error.h
@@ -1,14 +1,21 @@
-#ifndef MONETARY_ERROR_H
-#define MONETARY_ERROR_H
+#ifndef MONETARY_ERROR
+#define MONETARY_ERROR
 #include <stdexcept>
 #include <string>
 using namespace std;
 
-class monetary_error : public logic_error {
-public:
-   // Constructor
-   monetary_error(const string& msg) : logic_error(msg) {};
+cout << "HEJ" << endl;
+namespace monetary
+{
+class monetary_error : public logic_error 
+{
+ public:
+  explicit monetary_error(const string& what_arg) noexcept
+    : logic_error{what_arg}{}
+  
+  explicit monetary_error(const char* what_arg) noexcept
+    : logic_error{what_arg} {}
 };
- 
-#endif
 
+}
+#endif
