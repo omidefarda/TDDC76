bcd15e62464027b31579df6c82cd79070c6672b9
omidefarda <hasna648@student.liu.se>
2013-11-13 18:26:30 +0100

    Update Monetary.cc

--- Monetary.cc
+++ Monetary.cc
@@ -1,44 +1,58 @@
 #include "monetary_error.h"
-#include "Monetary.h"
+#include "monetary.h"
 #include <string>
 #include <iostream>
 using namespace std;
-//print osv
-void money::print(ostream &out)
+
+namespace monetary
 {
-  out << valuta << " " << enhet << "." << hundradelar << endl;
+  //print osv
+  void Money::print(ostream &out)
+  {
+    out << valuta << " " << enhet << "." << hundradelar;
+  }
+
+  //Sammansättning
+  Money& Money::operator+(Money& adderare)
+  {
+    if (valuta == adderare.valuta || valuta == "" || adderare.valuta == "")
+      {
+	enhet = enhet + adderare.enhet;
+	hundradelar = hundradelar + adderare.hundradelar;
+      }
+    else
+      {
+	throw monetary_error{"olika valuta"};
+      }
+    return *this;
+  }
+  Money& Money::operator=(Money& tilldelare)
+  {
+    if (valuta==tilldelare.valuta || tilldelare.valuta=="")
+      {
+	enhet= tilldelare.enhet;
+	hundradelar=tilldelare.hundradelar;
+      }
+    else if(valuta=="")
+      {
+	valuta=tilldelare.valuta;
+	enhet= tilldelare.enhet;
+	hundradelar=tilldelare.hundradelar;
+      }
+    else
+      {
+	throw monetary_error("olika valuta");
+      }
+    return *this;
+  }
+  ostream& operator<<(ostream& os,const Money& money_print)
+  {
+    os << money_print.valuta << " " << money_print.enhet << "." << money_print.hundradelar;
+    return os;
+  }
+
 }
 
-//Sammansättning
-money& money::operator+(money& adderare)
-{
-  if (valuta == adderare.valuta || valuta == "" || adderare.valuta == "")
-    {
-      enhet = enhet + adderare.enhet;
-      hundradelar = hundradelar + adderare.hundradelar;      
-    }
-  else
-    {
-      throw monetary_error ("olika valuta");
-    }
-}
-money& money::operator=(money& tilldelare)
-{
-  if (valuta==tilldelare.valuta || tilldelare.valuta=="")
-    {
-      enhet= tilldelare.enhet;
-      hundradelar=tilldelare.hundradelar;
-     }
-  else if(valuta=="")
-    {
-      valuta=tilldelare.valuta;
-      enhet= tilldelare.enhet;
-      hundradelar=tilldelare.hundradelar;
-    }      
-  else
-    {
-      throw monetary_error ("olika valuta");
-    }
-}
 //Jämförelse
 //bool operator=(const money&, const money&);
+
