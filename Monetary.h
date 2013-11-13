4551198148547e2c46d8b5970dab4c4daa30f065
omidefarda <hasna648@student.liu.se>
2013-11-13 18:27:26 +0100

    Update Monetary.h

--- Monetary.h
+++ Monetary.h
@@ -7,55 +7,75 @@
 
 using namespace std;
 
-
-class money
+namespace monetary
 {
-private:
-  string valuta;
-  int enhet;
-  int hundradelar;
-public:
-
-  // Konstruktor utan argument
-  money()
+  class Money
   {
-    this ->valuta="";
-    this ->enhet=0;
-    this ->hundradelar=0;      
-  }
-  // Konstruktor för enbart ints
-  explicit money(int enhet, int hundradelar=0)
-  {
-    this ->enhet=enhet;
-    this->hundradelar=hundradelar;
-  }  
-  
-  // Konstruktor för string +/- int
-  explicit money(string valuta, int enhet=0, int hundradelar=0)
-  {
-    this ->valuta=valuta;
-    this ->enhet=enhet;
-    this ->hundradelar=hundradelar; 
-  }
-  
-  // Koppieringskonstruktor
-  money(const money&)=default;  
-  //money(const char*);
-  ~money() = default;
-  money& operator=(const char*);
-  
-  //Print med ostream
-  void print(ostream &out);
-  
-  //Sammansättning
-  money& operator+(money&);
-  money& operator=(money&);
+  private:
+    string valuta;
+    int enhet;
+    int hundradelar;
+  public:
 
-  //Jämförelse
-  bool operator==(money&);
-  bool operator<(money&);
-};
+    // Konstruktor utan argument
+    Money()
+      {
+	this ->valuta="";
+	this ->enhet=0;
+	this ->hundradelar=0;
+      }
+    // Konstruktor för enbart ints
+    explicit Money(int enhet, int hundradelar=0)
+    {
+      this ->enhet=enhet;
+      this->hundradelar=hundradelar;
+    }
+  
+    // Konstruktor för string +/- int
+    explicit Money(string valuta, int enhet=0, int hundradelar=0)
+    {
+      this ->valuta=valuta;
+      this ->enhet=enhet;
+      this ->hundradelar=hundradelar;
+    }
+  
+    // Koppieringskonstruktor
+    Money(const Money&)=default;
+    //money(const char*);
+    ~Money() = default;
+    Money& operator=(const char*);
+  
+    //Print x2
+    void print(ostream &out);
+    friend ostream& operator<<(ostream& out, const Money&);
 
+    //Sammansättning
+    Money& operator+(Money&);
+    Money& operator=(Money&);
 
+    //Jämförelse
+    //Vad menas med "Implementera < och == "direkt" och definiera de övriga i termer av dess"
+    //är money++/++money här nedanför "direkt" implementerade och hur skall man då definiera de överiga i dessa?
+    bool operator==(Money&);
+    bool operator<(Money&);
+
+    //Stegning
+    //++money
+    Money& operator++()
+      {
+	hundradelar=hundradelar+1;
+	return *this;
+      }
+    //money++
+    Money operator++(int)
+    {
+      Money temp = *this;
+      hundradelar=hundradelar+1;
+      return temp;
+    }
+  };
+
+}
 
 #endif
+
