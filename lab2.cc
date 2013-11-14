a150b759b0c92d5661ea74bdb3ee91f5a756168d
omidefarda <hasna648@student.liu.se>
2013-11-14 16:49:20 +0100

    Update lab2.cc

--- lab2.cc
+++ lab2.cc
@@ -1,6 +1,9 @@
+
+
 #include <iostream>
 #include <string>
 #include "monetary.h"
+#include <stdexcept>
 
 using namespace std;
 using namespace monetary;
@@ -14,13 +17,19 @@ void stegning();
  
 int main(void)
 {
+  Money m11("SVENSKAKR");
+  Money m12;
   initiering();
   utskrift();
   tilldelning();
   addition();
+  jamforelse();
   stegning();
+  string valuta = m11.currency();
+  string valuta2 = m12.currency();
+  cout << valuta << endl;
+  cout << valuta2 << endl;
   return 0;
- 
 }
 
 void initiering()
@@ -49,10 +58,12 @@ void utskrift()
   Money m1;
   Money m3(10,50);
   Money m5("SEK", 10);
+  Money m10("SEK", 10 ,9);
   try
     {
       cout << "Testar utskrift" << endl;
       m5.print(cout);
+      m10.print(cout);
       cout << m5 << endl;
       cout << m3 << endl;
       cout << m1 << endl;
@@ -77,11 +88,10 @@ void tilldelning()
       m4 = m5;
       m4 = m3;
       m1 = m6;
-      m4 = m6;
     }
   catch (const monetary_error& e) 
     {
-      cout << e.what() << " (\"OK\")" << endl;
+      cout << e.what() << endl;
     } 
   cout << "Fungerade" << endl;
 }
@@ -109,11 +119,24 @@ void addition()
 void jamforelse()
 {
   Money m4 ("SEK");
-  Money m5 ("SEK", 10);
+  Money m5 ("SEK", 10, 50);
   Money m3 (10, 50);
+  int a;
   try 
     {
       cout << "Testar Jämförelse"<< endl;
+      a = m4 < m5;
+      cout << "Är m4 < m5" << " " << a << endl;
+      a = (m4 == m5);
+      cout << "Är m4 == m5" << " " << a << endl;
+      a = m3 > m5;
+      cout << "Är m3 > m5"  << " " << a << endl;
+      a = m3 != m5;
+      cout << "är m3 != m5" << " " << a << endl;
+      a = m3 >= m5;
+      cout << "är m3 >= m5" << " " << a << endl;
+      a = m5 <= m3;
+      cout << "är m5 <= m3" << " " << a << endl;
     }
   catch (const monetary_error& e) 
     {
@@ -142,6 +165,3 @@ void stegning()
 }
 
   
-
-
-
